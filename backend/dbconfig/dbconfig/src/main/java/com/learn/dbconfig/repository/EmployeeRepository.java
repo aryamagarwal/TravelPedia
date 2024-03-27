89f83818dccfe6e429a7a05e2e7f4e584286482d
aryamagarwal <mailaryam1000@gmail.com>
2024-03-27 13:14:55 +0530

    new folder structure

--- backend/dbconfig/dbconfig/src/main/java/com/learn/dbconfig/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.learn.dbconfig.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.learn.dbconfig.entity.employee;
-
-public interface EmployeeRepository extends JpaRepository<employee, Long>{
-
-}
