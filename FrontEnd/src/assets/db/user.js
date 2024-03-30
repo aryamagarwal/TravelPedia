9880363e04d272f52c5a27950ac2c63c89a40bd1
aryamagarwal <mailaryam1000@gmail.com>
2024-03-30 18:03:29 +0530

    Adding validation in LoginForm

---
+++ FrontEnd/src/assets/db/user.js
@@ -0,0 +1,32 @@
+const userData = [
+    {
+        "username": "aryam",
+        "firstName": "Aryam",
+        "LastName": "Agarwal",
+        "email": "lcs2023045@iiitl.ac.in",
+        "password": "Aa1234@"
+    },
+    {
+        "username": "priyanshu",
+        "firstName": "Priyanshu",
+        "LastName": "Agarwal",
+        "email": "lit2023022@iiitl.ac.in",
+        "password": "Pa1234@"
+    },
+    {
+        "username": "rewant",
+        "firstName": "Rewant",
+        "LastName": "Kurapati",
+        "email": "lit2023023@iiitl.ac.in",
+        "password": "Rk1234@"
+    },
+    {
+        "username": "akshat",
+        "firstName": "Akshat",
+        "LastName": "Sing",
+        "email": "lcs2023041@iiitl.ac.in",
+        "password": "As1234@"
+    }
+];
+
+export default userData;
--- FrontEnd/src/components/LoginFrom.jsx
+++ FrontEnd/src/components/LoginFrom.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { useState } from 'react';
 import { FaUser } from "react-icons/fa";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { Link } from 'react-router-dom';
+import userData from '../assets/db/user.js'
 import Block from './block';
 const LoginForm = () => {
+    const user=userData;
+    const [email , setEmail] = useState("");
+    const [password, setPassword]=useState("");
+    const updateEmail=(e)=>{
+         setEmail(e.target.value);
+    }
+    const updatePassword=(e)=>{
+        setPassword(e.target.value);
+   }
+   const validate=()=>{
+        const found=user.find((user)=>user.email===email);
+        console.log(found);
+        if(found)
+        {
+            if(found.password===password)
+            {
+              alert("Logged in successfully");
+            }
+            else
+            {
+                alert("Wrong email or password!");
+            }
+        }
+        else{
+            alert("Wrong email or password2");
+        }
+   }
     return (
         <>
         <Block></Block>
@@ -12,11 +41,11 @@ const LoginForm = () => {
             <form action="">
                 <h1 className="text-4xl text-center mb-5 font-bold">Login</h1>
                 <div className="inputBox flex relative items-center my-3 h-12 w-full ">
-                   <input className="bg-transparent w-full p-5 h-full text-xl rounded-3xl outline-none border-2 focus:border-red-400" type="email" placeholder="Email" required />
+                   <input className="bg-transparent w-full p-5 h-full text-xl rounded-3xl outline-none border-2 focus:border-red-400" type="email" placeholder="Email" required onChange={updateEmail} value={email}/>
                    <FaUser className="absolute right-5 top-1/2 -translate-y-2/4"/>
                 </div>
                 <div className="inputBox flex relative items-center my-3 h-12 w-full">
-                   <input className="bg-transparent w-full p-5 h-full text-xl rounded-3xl outline-none border-2 focus:border-red-400" type="password" placeholder="Password" required />
+                   <input className="bg-transparent w-full p-5 h-full text-xl rounded-3xl outline-none border-2 focus:border-red-400" type="password" placeholder="Password" required onChange={updatePassword} value={password} />
                    <RiLockPasswordFill className="absolute right-5 top-1/2 -translate-y-2/4"/>
                 </div>
                 <div className="rememberForgot flex justify-between my-3">
@@ -24,7 +53,7 @@ const LoginForm = () => {
                     <br/>
                     <Link to={"/forgotpassword"} href="#" className="hover:text-red-400 font-bold">Forgot Password?</Link>
                 </div>
-                <button className="w-full h-auto rounded-3xl font-bold hover:bg-red-600 bg-red-800 text-white p-2">Login</button>
+                <button className="w-full h-auto rounded-3xl font-bold hover:bg-red-600 bg-red-800 text-white p-2" onClick={validate}>Login</button>
                 <div className="register text-center mt-5 mb-10">
                     <p>Don't have an account? <Link to={"/SignUp"} href="#" className="hover:text-red-400 font-bold"> Register</Link> </p>
                 </div>
