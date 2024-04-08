69e60db2dc854a9c5bfc45f4cd30ac7160369d10
Luv888 <145419701+Luv888@users.noreply.github.com>
2024-04-08 14:12:49 +0530

    first commit

--- FrontEnd/src/Pages/Blogs.jsx
+++ FrontEnd/src/Pages/Blogs.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Blogs = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div>Akshat here</div>;
+};
 
-export default Blogs
+export default Blogs;
